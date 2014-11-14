e1b715f1f82788b6d585d438a348e43452e51967
Antti Salminen <antti.salminen@gmail.com>
2014-11-14 16:12:44 +0200

    Initial import.

---
+++ tests/test.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+SITE=test.yml
+ansible-galaxy install ajsalminen.avahi_common
+ansible-galaxy install ajsalminen.avahi_aliases
+ansible-galaxy install ajsalminen.httpd_site
+ansible-playbook -i $INVENTORY tests/$SITE --syntax-check
+ansible-playbook -i $INVENTORY tests/$SITE --connection=local --sudo
+  # Rerun for idempotency check.
+ansible-playbook -i $INVENTORY tests/$SITE --connection=local --sudo \
+| grep -q 'changed=0.*failed=0' \
+&& (echo 'Idempotence test: pass' && exit 0) \
+|| (echo 'Idempotence test: fail' && exit 1)
