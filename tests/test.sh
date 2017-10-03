4fcfd398c9de7bc6462cc1c47217d9cafc72309e
Joonas Kylmälä <joonas.kylmala@kirjastot.fi>
2017-10-03 14:49:50 +0300

    Use become instead of sudo.

--- tests/test.sh
+++ tests/test.sh
@@ -4,9 +4,9 @@ ansible-galaxy install ajsalminen.avahi_common
 ansible-galaxy install ajsalminen.avahi_aliases
 ansible-galaxy install ajsalminen.httpd_site
 ansible-playbook -i $INVENTORY tests/$SITE --syntax-check
-ansible-playbook -i $INVENTORY tests/$SITE --connection=local --sudo
+ansible-playbook -i $INVENTORY tests/$SITE --connection=local --become
   # Rerun for idempotency check.
-ansible-playbook -i $INVENTORY tests/$SITE --connection=local --sudo \
+ansible-playbook -i $INVENTORY tests/$SITE --connection=local --become \
 | grep -q 'changed=0.*failed=0' \
 && (echo 'Idempotence test: pass' && exit 0) \
 || (echo 'Idempotence test: fail' && exit 1)
